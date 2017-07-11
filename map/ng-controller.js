63abd657a6f61788d3301f76b1085b5d02dd3633
chingchai <chingchai.h@gmail.com>
2017-07-11 16:12:32 +0700

    add radar services
    add radar services

--- map/ng-controller.js
+++ map/ng-controller.js
@@ -2,19 +2,57 @@ angular.module('app.controller', ['ui-leaflet', 'ng-echarts'])
 
     .controller('mapCtrl', function ($scope) {
 
-        var radar_pl = {
-            name: 'ข้อมูลฝนจาก Radar: พิษณุโลก',
-            type: 'imageOverlay',
+        var radar_phs = {
+            name: 'ข้อมูลเรดาร์ฝน: พิษณุโลก',
+            type: 'wms',
             visible: true,
-            url: 'http://rain.tvis.in.th/output/PHS.png',
-            bounds: [[19.094393, 102.475537], [14.411350, 97.983591]],
+            url: 'http://www3.cgistln.nu.ac.th/geoserver/ows?',
             layerParams: {
-                noWrap: true,
-                zIndex: 2
+                layers: 'lsnanbasin:PHS',
+                format: 'image/png',
+                attribution: '&copy; <a href="http://rain.tvis.in.th">NECTEC&TMD</a> &copy; <a href="http://www.gistnu.com">GISTNU</a>',
+                transparent: true,
+                zIndex: 3
             },
             layerOptions: {
                 format: "image/png",
-                opacity: 0.7,
+                opacity: 1,
+            },
+            group: "Open Fire Map"
+        };
+        var radar_cri = {
+            name: 'ข้อมูลเรดาร์ฝน: เชียงราย',
+            type: 'wms',
+            visible: true,
+            url: 'http://www3.cgistln.nu.ac.th/geoserver/ows?',
+            layerParams: {
+                layers: 'lsnanbasin:CRI',
+                format: 'image/png',
+                attribution: '&copy; <a href="http://rain.tvis.in.th">NECTEC&TMD</a> &copy; <a href="http://www.gistnu.com">GISTNU</a>',
+                transparent: true,
+                zIndex: 3
+            },
+            layerOptions: {
+                format: "image/png",
+                opacity: 1,
+            },
+            group: "Open Fire Map"
+        };
+        var radar_kkn = {
+            name: 'ข้อมูลเรดาร์ฝน: ขอนแก่น',
+            type: 'wms',
+            visible: true,
+            url: 'http://www3.cgistln.nu.ac.th/geoserver/ows?',
+            layerParams: {
+                layers: 'lsnanbasin:KKN',
+                format: 'image/png',
+                attribution: '&copy; <a href="http://rain.tvis.in.th">NECTEC&TMD</a> &copy; <a href="http://www.gistnu.com">GISTNU</a>',
+                transparent: true,
+                zIndex: 3
+            },
+            layerOptions: {
+                format: "image/png",
+                opacity: 1,
             },
             group: "Open Fire Map"
         };
@@ -22,12 +60,12 @@ angular.module('app.controller', ['ui-leaflet', 'ng-echarts'])
             name: 'ขอบเขตจังหวัด',
             type: 'wms',
             visible: true,
-            url: 'http://map.nu.ac.th/gs-alr2/ows?',
+            url: 'http://ows.gistda.or.th/geoserver/ows?',
             layerParams: {
-                layers: 'alr:ln9p_prov',
+                layers: 'fgds54_beta:L05_AdminBoundary_Province_2011_50k_FGDS_beta',
                 format: 'image/png',
                 transparent: true,
-                CQL_FILTER: "prov_code IN ('53', '65')",
+                CQL_FILTER: "PRV_CODE IN ('53', '55', '65', '66')",
                 zIndex: 3
             },
             layerOptions: {
@@ -97,7 +135,7 @@ angular.module('app.controller', ['ui-leaflet', 'ng-echarts'])
             center: {
                 lat: 17.100,
                 lng: 100.200,
-                zoom: 8
+                zoom: 7
             },
             layercontrol: {
                 icons: {
@@ -108,13 +146,23 @@ angular.module('app.controller', ['ui-leaflet', 'ng-echarts'])
 
             layers: {
                 baselayers: {
-                    cycle: {
+                  cdb: {
+                      name: 'CartoDBMap',
+                      type: 'xyz',
+                      url: 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+                      layerOptions: {
+                          subdomains: ['a', 'b', 'c'],
+                          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OSM</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+                          continuousWorld: true
+                      }
+                  },
+                  cycle: {
                         name: 'OpenCycleMap',
                         type: 'xyz',
                         url: 'http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png',
                         layerOptions: {
                             subdomains: ['a', 'b', 'c'],
-                            attribution: '&copy; <a href="http://www.opencyclemap.org/copyright">OpenCycleMap</a> contributors - &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                            attribution: '&copy; <a href="http://www.opencyclemap.org/copyright">OCM</a> &copy; <a href="http://www.openstreetmap.org/copyright">OSM</a>',
                             continuousWorld: true
                         }
                     },
@@ -124,7 +172,7 @@ angular.module('app.controller', ['ui-leaflet', 'ng-echarts'])
                         url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
                         layerOptions: {
                             subdomains: ['a', 'b', 'c'],
-                            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OSM</a> contributors',
                             continuousWorld: true
                         }
                     }
@@ -132,7 +180,7 @@ angular.module('app.controller', ['ui-leaflet', 'ng-echarts'])
                 overlays: {
                     province: province
                 }
-            },           
+            },
 
             //village
             removeVillageLayer: function () {
@@ -170,14 +218,29 @@ angular.module('app.controller', ['ui-leaflet', 'ng-echarts'])
                 this.layers.overlays.province = province
             },
 
-            //pl radar
-            removeRadar_plLayer: function () {
-                delete this.layers.overlays.radar_pl;
+            //phs radar
+            removeRadar_phsLayer: function () {
+                delete this.layers.overlays.radar_phs;
             },
-            addRadar_plLayer: function () {
-                this.layers.overlays.radar_pl = radar_pl
+            addRadar_phsLayer: function () {
+                this.layers.overlays.radar_phs = radar_phs
             },
 
+            // cri radar
+            removeRadar_criLayer: function () {
+                delete this.layers.overlays.radar_cri;
+            },
+            addRadar_criLayer: function () {
+                this.layers.overlays.radar_cri = radar_cri
+            },
+
+            // kkn radar
+            removeRadar_kknLayer: function () {
+                delete this.layers.overlays.radar_kkn;
+            },
+            addRadar_kknLayer: function () {
+                this.layers.overlays.radar_kkn = radar_kkn
+            },
 
         })
 
@@ -186,9 +249,11 @@ angular.module('app.controller', ['ui-leaflet', 'ng-echarts'])
         $scope.checkboxModel = {
             vill: false,
             tambon: false,
-            amphoe: false,            
+            amphoe: false,
             province: true,
-            radar_pl: false
+            radar_phs: false,
+            radar_cri: false,
+            radar_kkn: false
         };
 
         $scope.showLayers = function (val) {
@@ -218,25 +283,37 @@ angular.module('app.controller', ['ui-leaflet', 'ng-echarts'])
                 } else {
                     $scope.removeProvinceLayer();
                 }
-            }else if (val == 'radar_pl') {
-                if ($scope.checkboxModel.radar_pl == true) {
-                    $scope.addRadar_plLayer();
+            }else if (val == 'radar_phs') {
+                if ($scope.checkboxModel.radar_phs == true) {
+                    $scope.addRadar_phsLayer();
                 } else {
-                    $scope.removeRadar_plLayer();
+                    $scope.removeRadar_phsLayer();
+                }
+            }else if (val == 'radar_cri') {
+                if ($scope.checkboxModel.radar_cri == true) {
+                    $scope.addRadar_criLayer();
+                } else {
+                    $scope.removeRadar_criLayer();
+                }
+            }else if (val == 'radar_kkn') {
+                if ($scope.checkboxModel.radar_kkn == true) {
+                    $scope.addRadar_kknLayer();
+                } else {
+                    $scope.removeRadar_kknLayer();
                 }
             }
         }
 
         $scope.barOption = {
             title : {
-                text: '蒸发量和降水量',
-                subtext: '纯属虚构'
+                text: '',
+                subtext: 'mm.'
             },
             tooltip : {
                 trigger: 'axis'
             },
             legend: {
-                data:['蒸发量','降水量']
+                data:['Evap','Rain']
             },
             toolbox: {
                 show : true,
@@ -252,7 +329,7 @@ angular.module('app.controller', ['ui-leaflet', 'ng-echarts'])
             xAxis : [
                 {
                     type : 'category',
-                    data : ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月']
+                    data : ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
                 }
             ],
             yAxis : [
@@ -262,34 +339,34 @@ angular.module('app.controller', ['ui-leaflet', 'ng-echarts'])
             ],
             series : [
                 {
-                    name:'蒸发量',
+                    name:'Evap',
                     type:'bar',
                     data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3],
                     markPoint : {
                         data : [
-                            {type : 'max', name: '最大值'},
-                            {type : 'min', name: '最小值'}
+                            {type : 'max', name: 'Max'},
+                            {type : 'min', name: 'Min'}
                         ]
                     },
                     markLine : {
                         data : [
-                            {type : 'average', name: '平均值'}
+                            {type : 'average', name: 'Avg'}
                         ]
                     }
                 },
                 {
-                    name:'降水量',
+                    name:'Rain',
                     type:'bar',
                     data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
                     markPoint : {
                         data : [
-                            {name : '年最高', value : 182.2, xAxis: 7, yAxis: 183, symbolSize:18},
-                            {name : '年最低', value : 2.3, xAxis: 11, yAxis: 3}
+                            {name : 'Max', value : 182.2, xAxis: 7, yAxis: 183, symbolSize:18},
+                            {name : 'Min', value : 2.3, xAxis: 11, yAxis: 3}
                         ]
                     },
                     markLine : {
                         data : [
-                            {type : 'average', name : '平均值'}
+                            {type : 'average', name : 'Avg'}
                         ]
                     }
                 }
