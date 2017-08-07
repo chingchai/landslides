3ae0adda1dd2b241e5b44fcb94d9948da458a6af
chingchai <chingchai.h@gmail.com>
2017-08-07 22:45:36 +0700

    add service wfs

--- map/ng-service.js
+++ map/ng-service.js
@@ -3,7 +3,7 @@ angular.module('app.service', [])
     return {
         selectedLocation: {},
         getWfs: function() {
-
+            // สำหรับ apache tomcat
             // www3 ติด 'Access-Control-Allow-Origin' วิธีแก้ตามนี้ https://gis.stackexchange.com/questions/210316/access-control-allow-origin-openlayers-wfs
             //var data =  'http://www3.cgistln.nu.ac.th/geoserver/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=gistdata:village&outputFormat=application%2Fjson';
             var data =  'http://www.map.nu.ac.th/gs-alr2/alr/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=alr:ln9p_vill&maxFeatures=50&outputFormat=application%2Fjson';
